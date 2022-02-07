4fa0bcc59e791c1c2102bbe1f4c512f56612ad85
ElenaLj <elenaljubijankic@gmail.com>
2022-02-07 16:44:28 +0100

    insert database columns

--- database/migrations/2022_02_07_144105_create_holidays_table.php
+++ database/migrations/2022_02_07_144105_create_holidays_table.php
@@ -15,6 +15,17 @@ class CreateHolidaysTable extends Migration
     {
         Schema::create('holidays', function (Blueprint $table) {
             $table->id();
+            $table->string("country", 200);
+            $table->string("city", 100);
+            $table->date("holiday_start");
+            $table->date("holiday_end");
+            $table->string("transport_type");
+            $table->boolean("transport_inclusion");
+            $table->string("hotel_address", 100);
+            $table->tinyInteger("hotel_rating")->unsigned();
+            $table->string("room_type", 20);
+            $table->smallInteger("price");
+            $table->boolean("confirmed");
             $table->timestamps();
         });
     }
